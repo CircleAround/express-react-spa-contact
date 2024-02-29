4b16806e5221ac84c83f8d0a8031826af23cc6f5
Masashi SATO <ms2.sato@gmail.com>
2024-02-29 09:10:26 +0900

    [backend] /contacts?limit=num

--- backend/src/routes/contacts.ts
+++ backend/src/routes/contacts.ts
@@ -4,11 +4,10 @@ import { getPrismaCilent } from "../lib/prisma-util";
 const prisma = getPrismaCilent();
 const router = express.Router();
 
-const take = 25;
-
 router.get("/", async (req, res, next) => {
   try {
     const page = req.query["page"] ? Number(req.query["page"] as string) : 1;
+    const take = req.query["limit"] ? Number(req.query["limit"] as string) : 25;
 
     const contacts = await prisma.contact.findMany({
       orderBy: { id: req.query["order"] == "desc" ? "desc" : "asc" },
